1946c261ed98d04e63b523d6431fd1e50e5e4ea5
oaguillon <aguillon_@hotmail.com>
2013-10-05 03:20:40 -0500

    doing push into repository

--- index.php
+++ index.php
@@ -11,25 +11,23 @@
 </head>
 <body>
 
-<div id="main">
-	<ul>
-		<li class="pages">1</li>
-		<li class="pages">2</li>
-		<li class="pages">3</li>
-		<li class="pages">4</li>
-		<li class="pages">5</li>
-		<li class="pages">6</li>
-		<li class="pages">7</li>
-		<li class="pages">8</li>
-	</ul>
-</div>
+	<div id="main">
+		<ul>
+			<li class="pages">1</li>
+			<li class="pages">2</li>
+			<li class="pages">3</li>
+			<li class="pages">4</li>
+			<li class="pages">5</li>
+			<li class="pages">6</li>
+			<li class="pages">7</li>
+			<li class="pages">8</li>
+		</ul>
+	</div>
 
-<div id="controles">
-<a href="#" id="back">Atras</a>
-<a href="#" id="next">Siguiente</a>
-</div>
-
-
+	<div id="controles">
+	<a href="#" id="back">Atras</a>
+	<a href="#" id="next">Siguiente</a>
+	</div>
 
 </body>
 </html>
